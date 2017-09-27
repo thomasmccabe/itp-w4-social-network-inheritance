027e49dbf1c1e629e9be30adb4671b913b8420ad
Thomas McCabe <tmccabe@stevens.edu>
2017-09-27 16:08:00 +0000

    Initial Commit for SocialClone

--- social_network/accounts.py
+++ social_network/accounts.py
@@ -1,13 +1,23 @@
 
 class User(object):
     def __init__(self, first_name, last_name, email):
-        pass
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.following = []
+        self.posts = []
 
     def add_post(self, post):
-        pass
+        post.set_user(self)
 
     def get_timeline(self):
-        pass
+        timeline = []
+        for people_being_followed in self.following:
+            for post in people_being_followed.posts:
+                timeline.append(post)
+                timeline.sort(key=lambda r: r.timestamp)    
+        return timeline
+
 
     def follow(self, other):
-        pass
+        self.following.append(other)
--- social_network/posts.py
+++ social_network/posts.py
@@ -1,33 +1,49 @@
 from datetime import datetime
 
+class Post(object):
+    def __init__(self, text, timestamp=None):
+        self.text = text
+        if timestamp == None:
+            self.timestamp = datetime.now().strftime("%A, %b %d, %Y") 
+            #ex: Tuesday, Jan 10, 2017
+        elif timestamp != None:
+            self.timestamp = timestamp.strftime("%A, %b %d, %Y")
+        self.post_info = {'text' : self.text, 'timestamp' : self.timestamp}
+        self.user = None
 
-# class Post(object):
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def set_user(self, user):
-#         pass
-
-
-# class TextPost(...):  # Inherit properly
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
-
-
-# class PicturePost(...):  # Inherit properly
-#     def __init__(self, text, image_url, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+    def set_user(self, user):
+        self.post_info['first_name'] = user.first_name
+        self.post_info['last_name'] = user.last_name
+        user.posts.append(self)
+        self.user = user
 
 
-# class CheckInPost(...):  # Inherit properly
-#     def __init__(self, text, latitude, longitude, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class TextPost(Post):  # Inherit properly
+    def __init__(self, text, timestamp=None):
+        super(TextPost, self).__init__(text, timestamp)
+        self.post_info = {'text' : self.text, 'timestamp' : self.timestamp}
+
+    def __str__(self):#how do I print out?
+        return '@{first_name} {last_name}: "{text}"\n\t{timestamp}'.format(**self.post_info)
+
+
+class PicturePost(Post):  # Inherit properly
+    def __init__(self, text, image_url, timestamp=None):
+        super(PicturePost, self).__init__(text, timestamp)
+        self.image_url = image_url
+        self.post_info = {'text' : self.text, 'timestamp' : self.timestamp, 'image_url' : image_url}
+
+    def __str__(self):
+        return '@{first_name} {last_name}: "{text}"\n\t{image_url}\n\t{timestamp}'.format(**self.post_info)
+
+
+class CheckInPost(Post):  # Inherit properly
+    def __init__(self, text, latitude, longitude, timestamp=None):
+        super(CheckInPost, self).__init__(text, timestamp)
+        self.latitude = latitude
+        self.longitude = longitude
+        self.post_info = {'text' : self.text, 'timestamp' : self.timestamp, 'latitude' : self.latitude, 'longitude' : self.longitude}
+
+    def __str__(self):
+        return '@{first_name} Checked In: "{text}"\n\t{latitude}, {longitude}\n\t{timestamp}'.format(**self.post_info)
+        
